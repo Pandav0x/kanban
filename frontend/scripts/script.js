0a5008162ca61e16d6f893865daa1242ed0de14d
Panda <pandav0x@hotmail.fr>
2020-02-25 07:37:33 +0100

    Move the display logic in a function (before segmenting it)

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -1,83 +1,7 @@
 let apiUrl = 'http://127.0.0.1:8000';
 
 document.addEventListener("DOMContentLoaded", function(){
-
-    let promise_statuses = new Promise((resolve) => { ajax('/status', 'GET', resolve); });
-
-    Promise.resolve(promise_statuses)
-        .then((unparsed_statuses) => {
-
-            let statuses = JSON.parse(unparsed_statuses);
-
-            statuses.forEach(function(status){
-                let status_column = document.createElement('div');
-                status_column.setAttribute('class', 'status-column');
-                status_column.setAttribute('id', 'status-' + status.id);
-                let status_tasks = document.createElement('ul');
-
-                let promise_tasks =  new Promise((resolve) => { ajax('/status/' + status.id + '/task', 'GET', resolve); });
-
-                Promise.resolve(promise_tasks).then((unparsed_tasks) => {
-                   let tasks = JSON.parse(unparsed_tasks);
-
-                   tasks.forEach(function(task){
-                       let task_element, task_name, task_element_content;
-
-                       if(task.project !== null){
-                           if(document.getElementById(status.name + '-' + task.project.name) !== null){
-
-                               //TODO: refactor those 4 lines (that are in the else as well)
-                               task_element = document.createElement('li');
-                               task_name = document.createTextNode(task.name);
-                               task_element_content = document.createElement('div');
-                               task_element_content.id = 'task-' + task.id;
-                               task_element_content.appendChild(task_name);
-                               task_element.appendChild(task_element_content);
-                               task_element.classList.add('task-element');
-                               task_element_content.setAttribute('draggable', 'true');
-                               document.getElementById(status.name + '-' + task.project.name).appendChild(task_element);
-
-                           } else {
-
-                               let project_container = document.createElement('li');
-                               let project_name_container = document.createElement('span');
-                               let project_name = document.createTextNode(task.project.name);
-                               project_name_container.appendChild(project_name);
-                               project_name_container.classList.add('project-title');
-                               project_container.appendChild(project_name_container);
-                               let project_container_content = document.createElement('ul');
-
-                               project_container_content.setAttribute('id', status.name + '-' + task.project.name);
-                               project_container_content.classList.add('project-container');
-                               task_element = document.createElement('li');
-                               task_element_content = document.createElement('div');
-                               task_element_content.id = 'task-' + task.id;
-                               task_name = document.createTextNode(task.name);
-                               task_element_content.setAttribute('draggable', 'true');
-                               task_element_content.appendChild(task_name);
-                               task_element.appendChild(task_element_content);
-                               task_element.classList.add('task-element');
-                               project_container_content.appendChild(task_element);
-
-                               project_container.appendChild(project_container_content);
-                               status_tasks.appendChild(project_container);
-                           }
-                       }
-                   });
-                    bindDragNDropEvents();
-                });
-
-                let status_inner_text_container = document.createElement('h2');
-                let status_inner_text = document.createTextNode(status.name);
-
-                status_inner_text_container.appendChild(status_inner_text);
-                status_column.appendChild(status_inner_text_container);
-                status_column.appendChild(status_tasks);
-
-                document.getElementById('main-content-container').appendChild(status_column);
-            });
-
-        });
+    display();
 });
 
 function ajax(url, protocol, callback)
@@ -111,6 +35,91 @@ function addTaskToStatus(task, status)
 function getBackendId(element)
 {
     return element.id.substr(element.id.lastIndexOf('-') + 1);
+}
+
+function display()
+{
+    let promise_statuses = new Promise((resolve) => { ajax('/status', 'GET', resolve); });
+
+    Promise.resolve(promise_statuses)
+        .then((unparsed_statuses) => {
+
+            let statuses = JSON.parse(unparsed_statuses);
+
+            statuses.forEach(function(status){
+                let status_column = document.createElement('div');
+                status_column.setAttribute('class', 'status-column');
+                status_column.setAttribute('id', 'status-' + status.id);
+                let status_tasks = document.createElement('ul');
+
+                let promise_tasks =  new Promise((resolve) => { ajax('/status/' + status.id + '/task', 'GET', resolve); });
+
+                Promise.resolve(promise_tasks).then((unparsed_tasks) => {
+                    let tasks = JSON.parse(unparsed_tasks);
+
+                    tasks.forEach(function(task){
+                        let task_element, task_name, task_element_content;
+
+                        if(task.project !== null){
+                            if(document.getElementById(status.name + '-' + task.project.name) !== null){
+
+                                //TODO: refactor those 4 lines (that are in the else as well)
+                                task_element = document.createElement('li');
+                                task_name = document.createTextNode(task.name);
+                                task_element_content = document.createElement('div');
+                                task_element_content.id = 'task-' + task.id;
+                                task_element_content.appendChild(task_name);
+                                task_element.appendChild(task_element_content);
+                                task_element.classList.add('task-element');
+                                task_element_content.setAttribute('draggable', 'true');
+                                document.getElementById(status.name + '-' + task.project.name).appendChild(task_element);
+
+                            } else {
+
+                                let project_container = document.createElement('li');
+                                let project_name_container = document.createElement('span');
+                                let project_name = document.createTextNode(task.project.name);
+                                project_name_container.appendChild(project_name);
+                                project_name_container.classList.add('project-title');
+                                project_container.appendChild(project_name_container);
+                                let project_container_content = document.createElement('ul');
+
+                                project_container_content.setAttribute('id', status.name + '-' + task.project.name);
+                                project_container_content.classList.add('project-container');
+                                task_element = document.createElement('li');
+                                task_element_content = document.createElement('div');
+                                task_element_content.id = 'task-' + task.id;
+                                task_name = document.createTextNode(task.name);
+                                task_element_content.setAttribute('draggable', 'true');
+                                task_element_content.appendChild(task_name);
+                                task_element.appendChild(task_element_content);
+                                task_element.classList.add('task-element');
+                                project_container_content.appendChild(task_element);
+
+                                project_container.appendChild(project_container_content);
+                                status_tasks.appendChild(project_container);
+                            }
+                        }
+                    });
+                    bindDragNDropEvents();
+                });
+
+                let status_inner_text_container = document.createElement('h2');
+                let status_inner_text = document.createTextNode(status.name);
+
+                status_inner_text_container.appendChild(status_inner_text);
+                status_column.appendChild(status_inner_text_container);
+                status_column.appendChild(status_tasks);
+
+                document.getElementById('main-content-container').appendChild(status_column);
+            });
+
+        });
+}
+
+function displayStatuses()
+{
+
 }
 
 function bindDragNDropEvents()
@@ -152,6 +161,7 @@ function bindDragNDropEvents()
             event.target.closest('.status-column').appendChild(dragged_element);
 
             addTaskToStatus(dragged_element, event.target.closest('.status-column'));
+            display();
 
             event.dataTransfer.clearData();
         });
