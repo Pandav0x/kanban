b78dafb332ba192b018b959707e1e9389c33a718
Panda <pandav0x@hotmail.fr>
2020-03-03 22:40:54 +0100

    Add double click edit + fix favicon + remove project url from base twig template

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,7 @@
-let serverUrl = null;
+let xml_parser = new XMLSerializer();
+let dom_parser = new DOMParser();
+
+let edited_elements = [];
 
 document.addEventListener("DOMContentLoaded", function(){
 
@@ -64,6 +67,121 @@ document.addEventListener('drop', function(event){
 
     event.dataTransfer.clearData();
 });
+
+//Edit
+document.addEventListener('dblclick', function(event) {
+    if(event.target.dataset.editable !== undefined)
+    {
+        let delete_button = createElement('span', '[X]', [
+            { 'class': 'delete-button' },
+            { 'data-target': event.target.id }
+        ]);
+
+        let validate_button = createElement('span', '[V]', [
+            { 'class': 'validate-button' },
+            { 'data-target': event.target.id }
+        ]);
+
+        let exit_button = createElement('span', '[~]', [
+            {'class': 'exit-button'}
+        ]);
+
+
+        let current_panel_id = getRandomString();
+        let edit_text_area = createElement('textarea', event.target.innerHTML);
+
+        let edit_component = createElement('div', null, [
+            {'class': 'edit-area'},
+            {'id': current_panel_id},
+            {'data-context': event.target.dataset.editable}
+        ]);
+
+        edit_component.appendChild(edit_text_area);
+        edit_component.appendChild(delete_button);
+        edit_component.appendChild(validate_button);
+        edit_component.appendChild(exit_button);
+
+        saveElement(current_panel_id, event.target, event.target.parentNode.id);
+
+        event.target.parentElement.replaceChild(edit_component, event.target);
+    }
+});
+
+document.addEventListener('click', function(event){
+    //TODO - refactor
+
+    if(event.target.closest('.exit-button')){
+        let edit_panel_id = event.target.parentNode.id;
+        if(edited_elements[edit_panel_id] !== null){
+
+            let edit_panel = document.getElementById(edit_panel_id);
+            let original_element = loadElement(edit_panel_id, 'element');
+
+            document.getElementById(loadElement(edit_panel_id, 'parent_id')).replaceChild(original_element, edit_panel);
+        }
+    }
+
+    if(event.target.closest('.validate-button')){
+        let edit_panel_id = event.target.parentNode.id;
+        let edit_context = event.target.parentNode.dataset.context;
+        console.log(edit_context);
+        if(edited_elements[edit_panel_id] !== null){
+
+            let edit_panel = document.getElementById(edit_panel_id);
+            let original_element = loadElement(edit_panel_id, 'element');
+            let parent_id = loadElement(edit_panel_id, 'parent_id');
+
+            original_element.innerText = edit_panel.querySelector('textarea').value;
+
+            let update_promise = new Promise((resolve) => {
+                ajax('/' + edit_context + '/update/' + getBackendId(parent_id) + '/' , 'PUT', resolve);
+            });
+
+            //TODO - ajax save
+
+            document.getElementById(parent_id).replaceChild(original_element, edit_panel);
+        }
+    }
+
+    if(event.target.closest('.delete-button')){
+        let edit_panel_id = event.target.parentNode.id;
+        let edit_context = event.target.parentNode.dataset.context;
+        if(edited_elements[edit_panel_id] !== null){
+            let parent_id = loadElement(edit_panel_id, 'parent_id');
+
+            document.getElementById(parent_id).remove();
+
+            console.log(getBackendId(parent_id));
+        }
+
+        //TODO - ajax delete
+
+    }
+});
+
+function saveElement(key, element, parent_id)
+{
+    edited_elements[key] = {
+        'element': xml_parser.serializeToString(element),
+        'parent_id': parent_id
+    };
+}
+
+function loadElement(key, flag)
+{
+    switch (flag){
+        case 'element':
+            return dom_parser.parseFromString(edited_elements[key].element, 'text/xml').documentElement;
+        case 'parent_id':
+            return edited_elements[key].parent_id;
+    }
+    return edited_elements[key];
+}
+
+function removeElement(key)
+{
+    delete edited_elements[key];
+}
 
 //Modal
 document.getElementById('button-add').addEventListener('click', function() {
@@ -189,13 +307,6 @@ function fetchModalSelectInfos()
     });
 }
 
-function getServerUrl()
-{
-    if(serverUrl === null)
-        serverUrl = document.getElementsByTagName('body')[0].dataset.serverUrl;
-    return serverUrl;
-}
-
 /**
  * @param url
  * @param method
@@ -214,13 +325,21 @@ function ajax(url, method, callback, data = null)
     }
 
     let xhr = new XMLHttpRequest();
-    xhr.open(method, getServerUrl() + url + formattedData);
+    xhr.open(method, url + formattedData);
     xhr.onload = function() {
         if (xhr.status === 200) {
             callback(xhr.responseText);
         }
     };
     xhr.send();
+}
+
+/**
+ * @returns {string}
+ */
+function getRandomString()
+{
+    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 }
 
 /**
@@ -231,7 +350,9 @@ function getBackendId(element)
 {
     if(element === null)
         return;
-    return element.id.substr(element.id.lastIndexOf('-') + 1);
+    if(typeof element === 'object')
+        return element.id.substr(element.id.lastIndexOf('-') + 1);
+    return element.substr(element.lastIndexOf('-') + 1);
 }
 
 /**
@@ -253,7 +374,11 @@ function displayStatuses()
                         {'id': 'status-' + status.id},
                         {'class': 'status-column'}
                     ]);
-                    status_column.appendChild(createElement('h2', status.name, [{'class': 'neon-white-red'}]));
+                    status_column.appendChild(createElement('h2', status.name, [
+                        {'class': 'neon-white-red'},
+                        {'id': 'status-' + status.id + '-title' },
+                        {'data-editable': 'status'}
+                    ]));
 
                     document.getElementById('main-content-container').appendChild(status_column);
                 }
@@ -289,16 +414,22 @@ function displayTasks()
                     {'class': 'project-container'}
                 ]);
 
-                task_project.appendChild(createElement('span', task.project.name, [{'class': 'project-title'}]));
+                task_project.appendChild(createElement('span', task.project.name, [
+                    {'class': 'project-title'},
+                    {'id': 'project-' + task.project.id + '-title'},
+                    {'data-editable': 'project'}
+                ]));
 
                 task_status.appendChild(task_project);
             }
 
-            let task_element = createElement('li');
+            let task_element = createElement('li', null, [{'id': 'task-' + task.id + '-container'}]);
+
             task_element.appendChild(createElement('div', task.name, [
                 {'id': 'task-' + task.id},
                 {'class': 'task-element'},
-                {'draggable': 'true'}
+                {'draggable': 'true'},
+                {'data-editable': 'task'}
             ]));
 
             if(document.querySelector('#task-' + task.id) !== null ||
