392665cf35a9bce832997286a00ac290c156bdaa
Panda <pandav0x@hotmail.fr>
2020-03-11 22:57:51 +0100

    Fix the fix that wasn't a fix

--- webpack.config.js
+++ webpack.config.js
@@ -56,7 +56,7 @@ Encore
         config.corejs = 3;
     })
     .addPlugin(new CopyWebpackPlugin([
-        { from: './assets/img', to: '../' }
+        { from: './assets/img', to: 'img' }
     ]))
 
 
